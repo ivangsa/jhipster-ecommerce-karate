49f6e1b3000e36145f4bd054431ff72879dc4c02
Ivan Garcia Sainz-Aja <ivangsa@ext.inditex.com>
2021-06-22 17:04:11 +0200

    adds minimal karate-config files

---
+++ jhipster-ecommerce-karate-tests/src/test/resources/karate-config.js
@@ -0,0 +1,15 @@
+function fn() {
+  function read(file) {
+    try {
+      return karate.read(file);
+    } catch (e) {
+      karate.log("File not found: " + file)
+      return {};
+    }
+  }
+
+  karate.set(read('classpath:config.yml'));
+  karate.set(read(`classpath:config-${karate.env}.yml`));
+
+  return { }
+}
